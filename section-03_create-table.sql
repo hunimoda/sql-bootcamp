d5cf6071dc323c334a4913279dc23c9b73ff3b3a
Hunimoda <hunimoda@gmail.com>
2022-06-18 10:02:52 +0900

    drop table

--- section-03_create-table.sql
+++ section-03_create-table.sql
@@ -4,12 +4,13 @@ CREATE TABLE cats(
     name VARCHAR(100),
     age INT);
 
-SHOW DATABASES;
+# SHOW DATABASES;
 SHOW TABLES;
-SHOW COLUMNS FROM cats;
-DESC cats;
+# SHOW COLUMNS FROM cats;
+# DESC cats;
 
 DROP TABLE cats;
 SHOW TABLES;
+# DESC cats; -> this should generate error
 
 DROP DATABASE cat_app;
