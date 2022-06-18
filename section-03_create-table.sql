f2e17bcf8ceee99e01daacd96e3ffedea7063171
Hunimoda <hunimoda@gmail.com>
2022-06-18 10:31:01 +0900

    code for create & drop table challenge

--- section-03_create-table.sql
+++ section-03_create-table.sql
@@ -1,16 +1,26 @@
-CREATE DATABASE cat_app;
-USE cat_app;
-CREATE TABLE cats(
-    name VARCHAR(100),
-    age INT);
+#CREATE DATABASE cat_app;
+#USE cat_app;
+#CREATE TABLE cats(
+#    name VARCHAR(100),
+#    age INT);
+#
+## SHOW DATABASES;
+#SHOW TABLES;
+## SHOW COLUMNS FROM cats;
+## DESC cats;
+#
+#DROP TABLE cats;
+#SHOW TABLES;
+## DESC cats; -> this should generate error
+#
+#DROP DATABASE cat_app;
 
-# SHOW DATABASES;
-SHOW TABLES;
-# SHOW COLUMNS FROM cats;
-# DESC cats;
-
-DROP TABLE cats;
-SHOW TABLES;
-# DESC cats; -> this should generate error
-
-DROP DATABASE cat_app;
+# create table challenge
+CREATE DATABASE challenge;
+USE challenge;
+CREATE TABLE pastries(
+    name VARCHAR(50),
+    quantity INT);
+DESC pastries;
+DROP TABLE pastries;
+DROP DATABASE challenge;
