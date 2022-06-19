e8c96a7e76e61e9fe7cc271d7e4eee6f2648cbfa
Hunimoda <hunimoda@gmail.com>
2022-06-19 22:42:25 +0900

    show warnings, make table column not null

--- section-04_insert-data.sql
+++ section-04_insert-data.sql
@@ -1,39 +1,52 @@
-#CREATE DATABASE cats_app;
-#USE cats_app;
+CREATE DATABASE cats_app;
+USE cats_app;
 #CREATE TABLE cats(name VARCHAR(50), age INT);
-#INSERT INTO cats(name, age)
-#VALUES ('Charlie', 10),
-#       ('Sadie', 3),
-#       ('Lazy Bear', 1),
-#       ('Blue', 2),
-#       ('Draco', 6);
-#SELECT * FROM cats;
-#DROP TABLE cats;
-#DROP DATABASE cats_app;
+CREATE TABLE cats(name VARCHAR(50) NOT NULL, age INT NOT NULL);
+INSERT INTO cats(name, age)
+VALUES ('Charlie', 10),
+       ('Sadie', 3),
+       ('Lazy Bear', 1),
+       ('Blue', 2),
+       ('Draco', 6);
 
-### insert challenge
-CREATE DATABASE challenge;
-USE challenge;
-CREATE TABLE people(first_name VARCHAR(20), last_name VARCHAR(20), age INT);
+# age not defined -> age becomes NULL
+INSERT INTO cats(name) VALUES ('Alabama');
+SHOW WARNINGS;
 
-# insert first person
-INSERT INTO people(first_name, last_name, age)
-    VALUES ('Tina', 'Belcher', 13);
-SELECT * FROM people;
+# name & age both not defined -> both become NULL
+INSERT INTO cats() VALUES ();
+SHOW WARNINGS;
 
-# insert second person in different order
-INSERT INTO people(last_name, age, first_name)
-    VALUES ('Belcher', 42, 'Bob');
+# both columns are NULL 'YES'
+DESC cats;
+
+SELECT * FROM cats;
+DROP TABLE cats;
+DROP DATABASE cats_app;
+
+#### insert challenge
+#CREATE DATABASE challenge;
+#USE challenge;
+#CREATE TABLE people(first_name VARCHAR(20), last_name VARCHAR(20), age INT);
+
+## insert first person
 #INSERT INTO people(first_name, last_name, age)
-#    VALUES ('Bob', 'Belcher', 42);
-SELECT * FROM people;
+#    VALUES ('Tina', 'Belcher', 13);
+#SELECT * FROM people;
 
-# multiple insert at once
-INSERT INTO people(first_name, last_name, age)
-VALUES ('Linda', 'Belcher', 45),
-       ('Phillip', 'Frond', 38),
-       ('Calvin', 'Fischoeder', 70);
-SELECT * FROM people;
+## insert second person in different order
+#INSERT INTO people(last_name, age, first_name)
+#    VALUES ('Belcher', 42, 'Bob');
+##INSERT INTO people(first_name, last_name, age)
+##    VALUES ('Bob', 'Belcher', 42);
+#SELECT * FROM people;
 
-DROP TABLE people;
-DROP DATABASE challenge;
+## multiple insert at once
+#INSERT INTO people(first_name, last_name, age)
+#VALUES ('Linda', 'Belcher', 45),
+#       ('Phillip', 'Frond', 38),
+#       ('Calvin', 'Fischoeder', 70);
+#SELECT * FROM people;
+
+#DROP TABLE people;
+#DROP DATABASE challenge;
